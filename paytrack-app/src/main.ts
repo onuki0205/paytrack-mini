4cfd6fe16b91e6cbfa0e8a83ff1457291e213511
onuki0205 <onknk.pl25@gmail.com>
2025-09-05 00:33:51 +0900

    vuetifyの導入

--- paytrack-app/src/main.ts
+++ paytrack-app/src/main.ts
@@ -1,5 +1,23 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// Vuetify関連のインポート
+import './styles.scss'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+// アイコンフォントのインポート
+import '@mdi/font/css/materialdesignicons.css' 
+
+// Vuetifyインスタンス作成
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+},
+})
+
+// VueアプリにVuetifyを適用
+createApp(App).use(vuetify).mount('#app')
